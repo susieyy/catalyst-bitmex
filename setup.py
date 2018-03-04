6e9bb36f0d4d474b3835ba22142c7eb39fdb960d
Daniel Zhou <nerdDan@users.noreply.github.com>
2018-03-04 20:09:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 		url='https://github.com/nerdDan/zipline_bitmex',
 		license='GPL',
 		author='Daniel Zhou',
-		author_email='i@danc.co',
+		author_email='danichau93@gmail.com',
 		description='BitMEX bundle for Zipline',
 		long_description=open('README.rst').read(),
 		install_requires=['requests', 'zipline>=1.1.1'],
